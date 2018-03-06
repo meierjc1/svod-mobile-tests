fc109c93800bcb653e5b38502e8f314aa31f563e
Jared Meier <jc@tech-jmeier.lan>
2018-03-06 14:56:38 -0800

    Added variables to adb statements

--- FillOutNewUserFields.sh
+++ FillOutNewUserFields.sh
@@ -1,7 +1,16 @@
-adb shell input text <first name>
+#! /bin/bash
+
+FIRSTNAME=JC
+LASTNAME=TEST
+EMAIL=jared.meier+030618_1445@feeln.com
+PASSWORD=Jaredtest1
+
+adb shell input text $FIRSTNAME
 adb shell input keyevent 20
-adb shell input text <last name>
+adb shell input text $LASTNAME
 adb shell input keyevent 20
-adb shell input text <email>
+adb shell input text $EMAIL
 adb shell input keyevent 20
-adb shell input text <password>
+adb shell input text $PASSWORD
+adb shell input keyevent 20
+adb shell input text $PASSWORD
