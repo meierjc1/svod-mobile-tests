af4c0c293ed6ea465e2726662d53db6e00999055
Jared Meier <jc@tech-jmeier.lan>
2018-03-06 15:05:29 -0800

    Added variables

--- deeplink.sh
+++ deeplink.sh
@@ -1,15 +1,20 @@
 #! /bin/bash
 
+IPADDRESS=10.20.5.148
+FILMID=13459
+EPISODEID=11239  #When looking at apify EPISODEID is the same as filmID
+SERIESID=195
+
 # Deeplink to a Film
-adb -s 192.168.86.30 shell am start -a android.intent.action.VIEW -n com.feeln.androidapp/com.feeln.android.tv.activity.SplashActivity -f 335544320 -e content_id 13459
+adb -s $IPADDRESS shell am start -a android.intent.action.VIEW -n com.feeln.androidapp/com.feeln.android.tv.activity.SplashActivity -f 335544320 -e content_id $FILMID
 
 # Wait 120 seconds
 sleep 120
 
 # Deeplink to a Tv Series
-adb -s 192.168.86.30 shell am start -a android.intent.action.VIEW -n com.feeln.androidapp/com.feeln.android.tv.activity.SplashActivity -f 335544320 -e content_id 11239
+adb -s $IPADDRESS shell am start -a android.intent.action.VIEW -n com.feeln.androidapp/com.feeln.android.tv.activity.SplashActivity -f 335544320 -e content_id $SERIESID
 
 # Wait 120 seconds
 sleep 120
 
-adb -s 192.168.86.30 shell am start -a android.intent.action.VIEW -n com.feeln.androidapp/com.feeln.android.tv.activity.SplashActivity -f 335544320 -e content_id s195
+adb -s $IPADDRESS shell am start -a android.intent.action.VIEW -n com.feeln.androidapp/com.feeln.android.tv.activity.SplashActivity -f 335544320 -e content_id s$SERIESID
